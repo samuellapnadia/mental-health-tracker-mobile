ab200d3af47b7e86b9592f3aeb0b96c4971fe7dc
samuellapnadia <samuellaputrinadiapauntu@gmail.com>
2024-11-13 12:18:56 +0700

    tutorial 8

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mental_health_tracker/screens/list_moodentry.dart';
 import 'package:mental_health_tracker/screens/menu.dart';
 import 'package:mental_health_tracker/screens/moodentry_form.dart';
 
@@ -39,29 +40,40 @@ class LeftDrawer extends StatelessWidget {
     ),
           ),
               ListTile(
-    leading: const Icon(Icons.home_outlined),
-    title: const Text('Home Page'),
-    // Redirection part to MyHomePage
-    onTap: () {
-      Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) => MyHomePage(),
-          ));
-    },
-  ),
-  ListTile(
-    leading: const Icon(Icons.mood),
-    title: const Text('Add Mood'),
-    // Redirection part to MoodEntryFormPage
-    onTap: () {
-      Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) => MoodEntryFormPage(),
-          ));
-    },
-  ),
+                leading: const Icon(Icons.home_outlined),
+                title: const Text('Home Page'),
+                // Redirection part to MyHomePage
+                onTap: () {
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MyHomePage(),
+                      ));
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.mood),
+                title: const Text('Add Mood'),
+                // Redirection part to MoodEntryFormPage
+                onTap: () {
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MoodEntryFormPage(),
+                      ));
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.add_reaction_rounded),
+                title: const Text('Mood List'),
+                onTap: () {
+                    // Route to the mood page
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const MoodEntryPage()),
+                    );
+                },
+            ),
         ],
       ),
     );
--- lib/widgets/mood_card.dart
+++ lib/widgets/mood_card.dart
@@ -1,43 +1,91 @@
-import 'package:mental_health_tracker/screens/moodentry_form.dart';
 import 'package:flutter/material.dart';
+import 'package:mental_health_tracker/screens/moodentry_form.dart';
+import 'package:mental_health_tracker/screens/list_moodentry.dart';
+import 'package:provider/provider.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:mental_health_tracker/screens/login.dart';
 
 class ItemHomepage {
-  final String name;
-  final IconData icon;
+    final String name;
+    final IconData icon;
 
-  ItemHomepage(this.name, this.icon);
+    ItemHomepage(this.name, this.icon);
 }
 
 class ItemCard extends StatelessWidget {
-  final ItemHomepage item;
+  // Display the card with an icon and name.
 
-  const ItemCard(this.item, {super.key});
+  final ItemHomepage item; 
+  
+  const ItemCard(this.item, {super.key}); 
 
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+
     return Material(
       color: Theme.of(context).colorScheme.secondary,
       borderRadius: BorderRadius.circular(12),
+      
       child: InkWell(
-        onTap: () {
+        onTap: () async {
+          // Display the SnackBar message when the card is pressed.
           ScaffoldMessenger.of(context)
             ..hideCurrentSnackBar()
             ..showSnackBar(
-              SnackBar(content: Text("You have pressed the ${item.name} button!")),
+              SnackBar(content: Text("You have pressed the ${item.name} button!"))
             );
-          if (item.name == "Add Mood") {
+
+              // Navigate to the appropriate route (depending on the button type)
+      if (item.name == "Add Mood") {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MoodEntryFormPage(),
+          ),
+        );
+      } else if (item.name == "View Mood") {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const MoodEntryPage(),
+          ),
+        );
+      } else if (item.name == "Logout") {
+        final response = await request.logout(
+            "http://127.0.0.1:8000/auth/logout/");
+        String message = response["message"];
+        
+        if (context.mounted) {
+          if (response['status']) {
+            String uname = response["username"];
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text("$message Goodbye, $uname."),
+              ),
+            );
             Navigator.pushReplacement(
               context,
-              MaterialPageRoute(
-                builder: (context) => MoodEntryFormPage(),
+              MaterialPageRoute(builder: (context) => const LoginPage()),
+            );
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(message),
               ),
             );
           }
+        }
+      }
+
+
         },
+        // Container to store the Icon and Text
         child: Container(
           padding: const EdgeInsets.all(8),
           child: Center(
             child: Column(
+              // Place the Icon and Text in the center of the card.
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Icon(
@@ -58,4 +106,5 @@ class ItemCard extends StatelessWidget {
       ),
     );
   }
+  
 }
